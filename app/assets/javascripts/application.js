28513baeda3eab5cb9124d7a81f24f55aec81a84
My Name <dodd.jl@gmail.com>
2014-04-07 17:06:41 -0400

    adding backbone

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require underscore
+//= require backbone
+//= require_tree ./backbone/models
+//= require_tree ./backbone/collections
+//= require_tree ./backbone/views
+//= require_tree ./backbone/routers
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,9 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+
   private
 
   def user_params
