b52f37ac2f9c1f70b5a57757d0da677f7bda4f03
My Name <dodd.jl@gmail.com>
2014-04-07 12:05:08 -0400

    first commit. Readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# The Daily Badge
+
+The Daily Badge is a way to set and organize goals and then gamify the process of acheiving them.
+ 
+"Badgers" take and post photos as proof to earn/claim badges.
+ 
+Every day the app will post a daily badge for badgers to earn (to keep people coming back!)
+
+
+
+**Check out the stories on Pivotal Tracker**
+[The Daily Badge](https://www.pivotaltracker.com/s/projects/1053290 "User Stories")
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thedailybadge::Application.config.secret_key_base = 'c3f6724ee2f40a73b3e6946ea014f16cd50c45ca1af4d5fcbd3b0e09e78ed7a3d9cad92fc9814918c24cd736fde1a728af1375eb13569b874e9b0095c36809cb'
